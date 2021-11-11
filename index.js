7e7c4c151ab0c29ed1d1639dc93df9e6b2be11b3
Celdama <celdama.dev@gmail.com>
2021-11-11 11:22:48 +0100

    last commit

--- index.js
+++ index.js
@@ -1,49 +1,60 @@
-const links = Array.from(document.querySelectorAll('.link'));
-const app = document.querySelector('#app');
-
-const changeBackgroundColor = (color) => {
-  app.style.backgroundColor = `${color}`;
-};
-
-const resetActiveAttr = (elements) => {
-  elements.forEach((element) => {
-    const elementToReset = element;
-    elementToReset.setAttribute('active', false);
-    elementToReset.style.color = '';
-  });
-};
-
-const changeColorLink = (link, color) => {
-  const elementToChangeColor = link;
-  elementToChangeColor.style.color = color;
-};
-
-const resetActiveSpan = () => {
-  const spans = Array.from(document.querySelectorAll('.span-link'));
-  spans.forEach((span) => {
-    if (span.classList.contains('active-span')) {
-      span.classList.remove('active-span');
-    }
-  });
-};
-
-const effectOnWrapperMenu = (itemClicked) => {
+const animateMobileMenu = (() => {
+  const links = Array.from(document.querySelectorAll('.link'));
+  const app = document.querySelector('#app');
   const wrapper = document.querySelector('.wrapper-menu');
 
-  wrapper.classList.toggle(`${itemClicked.classList[1]}-effect`);
-  setTimeout(() => {
-    wrapper.classList.toggle(`${itemClicked.classList[1]}-effect`);
-  }, 500);
-};
+  const changeBackgroundColor = (color) => {
+    app.style.backgroundColor = `${color}`;
+  };
 
-links.forEach((link) => {
-  link.addEventListener('click', () => {
-    resetActiveAttr(links);
-    resetActiveSpan();
-    changeBackgroundColor(link.dataset.color);
-    link.setAttribute('active', true);
-    link.childNodes[3].classList.toggle('active-span');
-    changeColorLink(link, link.dataset.color);
-    effectOnWrapperMenu(link);
-  });
-});
+  const resetActiveAttr = (elements) => {
+    elements.forEach((element) => {
+      const elementToReset = element;
+      elementToReset.setAttribute('active', false);
+      elementToReset.style.color = '';
+    });
+  };
+
+  const changeColorLink = (link, color) => {
+    const elementToChangeColor = link;
+    elementToChangeColor.style.color = color;
+  };
+
+  const resetActiveLink = () => {
+    const spans = Array.from(document.querySelectorAll('.span-link'));
+    spans.forEach((span) => {
+      if (span.classList.contains('active-span')) {
+        span.classList.remove('active-span');
+      }
+    });
+  };
+
+  const effectOnWrapperMenu = (itemClicked) => {
+    wrapper.classList.toggle(`${itemClicked.classList[1]}-effect`);
+    setTimeout(() => {
+      wrapper.classList.toggle(`${itemClicked.classList[1]}-effect`);
+    }, 500);
+  };
+
+  const animation = () => {
+    links.forEach((link) => {
+      link.addEventListener('click', () => {
+        resetActiveAttr(links);
+        resetActiveLink();
+        changeBackgroundColor(link.dataset.color);
+        link.setAttribute('active', true);
+        link.childNodes[3].classList.toggle('active-span');
+        changeColorLink(link, link.dataset.color);
+        effectOnWrapperMenu(link);
+      });
+    });
+  };
+
+  return {
+    animation,
+  };
+})();
+
+window.onload = () => {
+  animateMobileMenu.animation();
+};
